a59251b8f950d7f5d5a5790e7ce3776141e54ac1
baobab <18167993+baobab-it@users.noreply.github.com>
2020-08-29 12:46:41 +0300

    update

--- Ch01/src/bin/concat.rs
+++ Ch01/src/bin/concat.rs
@@ -5,40 +5,53 @@
 */
 
 fn main() {
-  by_moving(); // Віддаємо перевагу цьому способу, використовує менше памяті, діє прямолінійно
-  by_cloning();// коли потрібно залишити дані недоторканими, проте може використовувати багато памяті
-  by_mutating(); // варіант, який продуктивно працює як в першому випадку, але мутує змінну. Використовуємо у випадку великої потреби мутації даних чи в представлені стану в дуже малому і керованому контексті.
+    by_moving(); // Віддаємо перевагу цьому способу, використовує менше памяті, діє прямолінійно
+    by_cloning();// коли потрібно залишити дані недоторканими, проте може використовувати багато памяті
+    by_mutating(); // варіант, який продуктивно працює як в першому випадку, але мутує змінну. Використовуємо у випадку великої потреби мутації даних чи в представлені стану в дуже малому і керованому контексті.
+    let hello_world = "Hello world!".to_string();
+    println!("index first word {}", first_word(&hello_world)); // виводимо позицію першого слова
 }
 
 fn by_moving() {
-  let hello = "hello ".to_string(); // :String
-  let world = "world!";             // :&str
+    let hello = "hello ".to_string(); // :String
+    let world = "world!";             // :&str
 
-  // Переміщуємо hello до нової змінної
-  let hello_world = hello + world;
-  // Hello НЕ МОЖЕ більше використовуватись
-  //println!("{}", hello);// panic
-  println!("{}", world);
-  println!("{}", hello_world); // -> "hello world!"
+    // Переміщуємо hello до нової змінної
+    let hello_world = hello + world;
+    // Hello НЕ МОЖЕ більше використовуватись
+    //println!("{}", hello);// panic
+    println!("{}", world);
+    println!("{}", hello_world); // -> "hello world!"
 }
 
 fn by_cloning() {
-  let hello = "hello ".to_string(); // :String
-  let world = "world!";             // :&str
+    let hello = "hello ".to_string(); // :String
+    let world = "world!";             // :&str
 
-  // Створюємо копію hello і переміщуємо в нову змінну
-  let hello_world = hello.clone() + world;
-  // Hello може далі використовуватись
-  println!("{}", hello_world); // Друкуємо "hello world!"
+    // Створюємо копію hello і переміщуємо в нову змінну
+    let hello_world = hello.clone() + world;
+    // Hello може далі використовуватись
+    println!("{}", hello_world); // Друкуємо "hello world!"
 }
 
 fn by_mutating() {
-  let mut hello = "hello ".to_string();
-  let world = "world!";
+    let mut hello = "hello ".to_string();
+    let world = "world!";
 
-  // Модифікуємо hello
-  hello.push_str(world);
-  // hello містить два рядка
-  println!("{}", world);
-  println!("{}", hello); // Друкуємо "hello world!"
+    // Модифікуємо hello
+    hello.push_str(world);
+    // hello містить два рядка
+    println!("{}", world);
+    println!("{}", hello); // Друкуємо "hello world!"
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes(); // отримуємо масив байтів (УВАГА використовуємо тільки для Anscii кодувань)
+
+    for (i, &item) in bytes.iter().enumerate() { // enumerate обгортає ітератор і викодить кортеж (index, &item)
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
